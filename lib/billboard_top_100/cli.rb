e75460ce96f908d2deee49e1df4a389bf6fa8423
Adam Taitano <adam.taitano@gmail.com>
2016-07-15 13:09:56 -0700

    Completes initial tests for CLI

--- lib/billboard_top_100/cli.rb
+++ lib/billboard_top_100/cli.rb
@@ -1,2 +1,9 @@
 class BillboardTop100::CLI
+
+  def call
+    start
+  end
+
+  def start
+  end
 end
--- spec/billboard_top_100_spec.rb
+++ spec/billboard_top_100_spec.rb
@@ -1,90 +1,132 @@
 require 'spec_helper'
+#
+# describe BillboardTop100 do
+#   it 'has a version number' do
+#     expect(BillboardTop100::VERSION).not_to be nil
+#   end
+# end
+#
+# describe BillboardTop100::Song do
+#   context 'Class Methods' do
+#     describe '.all' do
+#       it 'lists all the songs in the top 100' do
+#         array = Array.new(100, "Song")
+#         expect(BillboardTop100::Song.all).to eql(array)
+#       end
+#     end
+#   end
+#
+#   context 'Instance Methods' do
+#     describe '#title' do
+#       song = BillboardTop100::Song.new("Sexyback")
+#       it 'has a song title' do
+#         expect(song.title).to eq("Sexyback")
+#       end
+#     end
+#     describe '#artist' do
+#       song = BillboardTop100::Song.new("Sexyback")
+#       song.artist = "Justin Timberlake"
+#       it 'has an artist' do
+#         expect(song.artist).to eq("Justin Timberlake")
+#       end
+#     end
+#     describe '#position' do
+#       song = BillboardTop100::Song.new("Sexyback")
+#       song.position = 1
+#       it 'has a position' do
+#       expect(song.position).to eq(1)
+#       end
+#     end
+#     describe '#peak' do
+#       song = BillboardTop100::Song.new("Sexyback")
+#       song.peak = 1
+#       it 'has a peak position' do
+#       expect(song.peak).to eq(1)
+#       end
+#     end
+#     describe '#last_week_position' do
+#       song = BillboardTop100::Song.new("Sexyback")
+#       song.last_week_position = 1
+#       it 'has a peak position' do
+#       expect(song.last_week_position).to eq(1)
+#       end
+#     end
+#     describe '#weeks_on_chart' do
+#       song = BillboardTop100::Song.new("Sexyback")
+#       song.weeks_on_chart = 1
+#       it 'has a peak position' do
+#       expect(song.weeks_on_chart).to eq(1)
+#       end
+#     end
+#   end
+# end
+#
+# describe BillboardTop100::Scraper do
+#
+#   describe "#get_page" do
+#     it 'gets the page' do
+#       expect(BillboardTop100::Scraper.new.get_page).should_not be(nil)
+#     end
+#   end
+#   describe '#collect_songs' do
+#     it 'collects 100 songs' do
+#       doc = BillboardTop100::Scraper.new
+#       doc.collect_songs
+#       expect(doc.display_songs.length).to eq(100)
+#     end
+#     it 'creates an array of hashes' do
+#       doc = BillboardTop100::Scraper.new
+#       doc.collect_songs
+#       expect(doc.display_songs).to be_instance_of(Array)
+#       expect(doc.display_songs[0]).to be_instance_of(Hash)
+#     end
+#     it 'includes attributes' do
+#       doc = BillboardTop100::Scraper.new
+#       doc.collect_songs
+#       expect(doc.display_songs[0]).to have_key(:title)
+#     end
+#   end
+#
+# end
 
-describe BillboardTop100 do
-  it 'has a version number' do
-    expect(BillboardTop100::VERSION).not_to be nil
-  end
-end
+describe BillboardTop100::CLI do
 
-describe BillboardTop100::Song do
-  context 'Class Methods' do
-    describe '.all' do
-      it 'lists all the songs in the top 100' do
-        array = Array.new(100, "Song")
-        expect(BillboardTop100::Song.all).to eql(array)
-      end
+  describe '#call' do
+    it 'calls upon #start' do
+      cli = BillboardTop100::CLI.new
+      expect(cli).to receive(:start)
+      cli.call
     end
   end
-
-  context 'Instance Methods' do
-    describe '#title' do
-      song = BillboardTop100::Song.new("Sexyback")
-      it 'has a song title' do
-        expect(song.title).to eq("Sexyback")
-      end
+  describe '#start' do
+    it 'should introduce' do
+      expect(STDOUT).to receive(:puts).with("Welcome to the Billboard Top 100 Songs!")
+      require_relative './../billboard_top_100/cli.rb'
     end
-    describe '#artist' do
-      song = BillboardTop100::Song.new("Sexyback")
-      song.artist = "Justin Timberlake"
-      it 'has an artist' do
-        expect(song.artist).to eq("Justin Timberlake")
-      end
+    it 'should ask for input' do
+      STDIN.should_receive(:gets)
     end
-    describe '#position' do
-      song = BillboardTop100::Song.new("Sexyback")
-      song.position = 1
-      it 'has a position' do
-      expect(song.position).to eq(1)
-      end
-    end
-    describe '#peak' do
-      song = BillboardTop100::Song.new("Sexyback")
-      song.peak = 1
-      it 'has a peak position' do
-      expect(song.peak).to eq(1)
-      end
-    end
-    describe '#last_week_position' do
-      song = BillboardTop100::Song.new("Sexyback")
-      song.last_week_position = 1
-      it 'has a peak position' do
-      expect(song.last_week_position).to eq(1)
-      end
-    end
-    describe '#weeks_on_chart' do
-      song = BillboardTop100::Song.new("Sexyback")
-      song.weeks_on_chart = 1
-      it 'has a peak position' do
-      expect(song.weeks_on_chart).to eq(1)
-      end
+    it 'should ask if user is done' do
+      expect(STDOUT).to receive(:puts).with("Would you like to see more songs? Enter 'Y' or 'N'")
+      require_relative './../billboard_top_100/cli.rb'
     end
   end
-end
-
-describe BillboardTop100::Scraper do
-  
-  describe "#get_page" do
-    it 'gets the page' do
-      expect(BillboardTop100::Scraper.new.get_page).should_not be(nil)
+  describe '#print_songs' do
+    cli = BillboardTop100::CLI.new
+    it 'should print formatted songs' do
+      expect(cli.print_songs).to include("1. One Dance - Drake Featuring WizKid & Kyla")
+    end
+    it 'should accept an argument of number(s) integer' do
+      expect { cli.print_songs(1) }.to_not raise_error
     end
   end
-  describe '#collect_songs' do
-    it 'collects 100 songs' do
-      doc = BillboardTop100::Scraper.new
-      doc.collect_songs
-      expect(doc.display_songs.length).to eq(100)
+  describe '#print_song' do
+    cli = BillboardTop100::CLI.new
+    it 'should accept a specific chart number' do
+      expect { cli.print_song(1) }.to_not raise_erro
     end
-    it 'creates an array of hashes' do
-      doc = BillboardTop100::Scraper.new
-      doc.collect_songs
-      expect(doc.display_songs).to be_instance_of(Array)
-      expect(doc.display_songs[0]).to be_instance_of(Hash)
-    end
-    it 'includes attributes' do
-      doc = BillboardTop100::Scraper.new
-      doc.collect_songs
-      expect(doc.display_songs[0]).to have_key(:title)
+    it 'should print details about the song' do
+      expect(cli.print_songs).to include("Last Week's Position = 1")
     end
   end
-
 end
