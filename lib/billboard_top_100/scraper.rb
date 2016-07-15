dba440a9cdff26d38cc91828e209a0b942b87d83
Adam Taitano <adam.taitano@gmail.com>
2016-07-15 12:26:27 -0700

    Complete initial Scraper Class

--- lib/billboard_top_100/scraper.rb
+++ lib/billboard_top_100/scraper.rb
@@ -8,16 +8,15 @@ class BillboardTop100::Scraper
 
   def collect_songs
     document = Nokogiri::HTML(open("http://www.billboard.com/charts/hot-100"))
-    songs = document.css("article.chart-row").collect do |song|
+    @@songs = document.css("article.chart-row").collect do |song|
       title = song.css("h2.chart-row__song").text
       artist = song.css("a.chart-row__artist").text
       position = song.css("span.chart-row__current-week").text
       peak = song.css(".chart-row__top-spot .chart-row__value").text
       last_week_position = song.css(".chart-row__last-week .chart-row__value").text
       weeks_on_chart = song.css(".chart-row__weeks-on-chart .chart-row__value").text
-      song
+      {:title => title, :artist => artist, :position => position, :peak => peak, :last_week_position => last_week_position, :weeks_on_chart => weeks_on_chart}
     end
-    @@songs << songs
   end
 
   def display_songs
